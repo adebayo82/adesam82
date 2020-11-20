17a29ee831aade798a920bfbe1677783a70febda
adebayo82 <Mary4love82@gmail.com>
2020-11-20 14:04:47 +0100

    third commit

---
+++ form.js
@@ -0,0 +1,35 @@
+let formSubmit = document.getElementById("formSubmit");
+formSubmit.addEventListener("submit", subimtData);
+function subimtData(){alert()}
+
+
+var a = 7;
+var c = 9;
+var b = (a*c) + (a*4);
+var d = b % 7;
+d += 10;
+a = d - c;
+console.log(a)
+
+
+const submitForm = document.getElementById("subJect");
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2")
+
+submitForm.addEventListener("submit, createuser")
+
+function createuser(e) {
+    // alert()
+    localStorage.setItem("register", ~${form1.value} ${form2.value}~)
+    form1.value
+    console.log(form1.value)
+
+    e.preventDefault();
+    location.href = "awakecolm.html"
+
+    if (form1.value == "||form2.value=="){
+
+    }
+    alert("type something")
+}
+
